e88c09e1b88eebd530f1d54d2c82f1a192b154e7
Ting WANG <skywy000@gmail.com>
2026-06-14 21:16:14 +0200

    complete lesson 1 agentic rag

--- 01-agentic-rag/code/rag_helper.py
+++ 01-agentic-rag/code/rag_helper.py
@@ -1,1 +1,88 @@
 # begin at 1.8 rag helper
+
+INSTRUCTIONS = '''
+Your task is to answer questions from the course participants
+based on the provided context.
+
+Use the context to find relevant information and provide accurate
+answers. If the answer is not found in the context,
+respond with "I don't know."
+'''
+
+PROMPT_TEMPLATE = '''
+QUESTION: {question}
+
+CONTEXT:
+{context}
+'''.strip()
+
+
+class RAGBase:
+
+    def __init__(
+        self,
+        index,
+        llm_client,
+        instructions=INSTRUCTIONS,
+        prompt_template=PROMPT_TEMPLATE,
+        course='llm-zoomcamp',
+        model='gemini-3.5-flash'
+    ):
+        self.index = index
+        self.llm_client = llm_client
+        self.instructions = instructions
+        self.course = course
+        self.prompt_template = prompt_template
+        self.model = model
+
+    def search(self, query, num_results=5):
+        boost_dict = {'question': 3.0, 'section': 0.5}
+        filter_dict = {'course': self.course}
+
+        return self.index.search(
+            query,
+            num_results=num_results,
+            boost_dict=boost_dict,
+            filter_dict=filter_dict
+        )
+
+    def build_context(self, search_results):
+        lines = []
+
+        for doc in search_results:
+            lines.append(doc['section'])
+            lines.append('Q: ' + doc['question'])
+            lines.append('A: ' + doc['answer'])
+            lines.append('')
+
+        return '\n'.join(lines).strip()
+
+    def build_prompt(self, query, search_results):
+        context = self.build_context(search_results)
+        return self.prompt_template.format(
+            question=query, context=context
+        )
+
+    def llm(self, prompt):
+        from google.genai import types
+        input_messages = [
+            {'role': 'developer', 'parts': [{ 'text': self.instructions }]},
+            {'role': 'user', 'parts': [{ 'text': prompt }]}
+        ]
+
+        response = self.llm_client.models.generate_content(
+            model=self.model,
+            contents=input_messages,
+            config=types.GenerateContentConfig(
+                system_instruction=self.instructions,      # = le rôle "developer"/"system" chez OpenAI
+                thinking_config=types.ThinkingConfig(thinking_budget=0)
+    )
+        )
+
+        return response.text
+
+    def rag(self, query):
+        search_results = self.search(query)
+        prompt = self.build_prompt(query, search_results)
+        answer = self.llm(prompt)
+        return answer
--- 01-agentic-rag/code/rag_ingest.py
+++
@@ -1,1 +0,0 @@
-# begin at 1.8 rag helper
